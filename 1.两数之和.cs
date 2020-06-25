de4536fd8b0c736b5954a8fc41ec67231922db7d
HysenChan <HysenChan@163.com>
2020-06-25 23:24:47 +0800

    【修改】添加两数之和另外两种方法

--- 1.两数之和.cs
+++ 1.两数之和.cs
@@ -9,6 +9,59 @@ using System;
 using System.Collections.Generic;
 public class Solution
 {
+    /// <summary>
+    /// 方法1：暴力法
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    // public static int[] TwoSum(int[] nums, int target)
+    // {
+    //     for (int i = 0; i < nums.Length; i++)
+    //     {
+    //         for (int j = i+1; j < nums.Length; j++)
+    //         {
+    //             if (nums[i] + nums[j] == target) return new int[] { i, j };
+    //         }
+    //     }
+    //     return new int[] { 0,0 };
+    // }
+
+    ///<summary>
+    /// 方法2：两遍哈希表
+    /// </summary>
+    /*public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> kvs = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (kvs.ContainsKey((nums[i])))
+            {
+                if (nums[i] * 2 == target)
+                {
+                    return new int[] { i, kvs[nums[i]] };
+                }
+            }
+            else
+            {
+                kvs.Add(nums[i], i);
+            }
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (kvs.ContainsKey(complement) && kvs[complement] != i)
+            {
+                return new int[] { i, kvs[complement] };
+            }
+        }
+        return new int[] { 0, 0 };
+    }*/
+
+    ///<summary>
+    /// 方法3：一遍哈希表
+    /// </summary>
     public static int[] TwoSum(int[] nums, int target)
     {
         Dictionary<int, int> kvs = new Dictionary<int, int>();
